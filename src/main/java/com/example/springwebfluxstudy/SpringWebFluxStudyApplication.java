e14d17e6a657713270937ccd8b2fbcd8888c703a
ori0o0p <openwund8517@gmail.com>
2023-09-14 19:40:35 +0900

    first commit

---
+++ src/main/java/com/example/springwebfluxstudy/SpringWebFluxStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebfluxstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebFluxStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springwebfluxstudy/SpringWebFluxStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebfluxstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFluxStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
